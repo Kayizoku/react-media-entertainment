f488e0e6639a7b8a9830a3a4799c9519cc374af6
Mohammad Tayyab Khalid <tayyabkhalid306@gmail.com>
2021-12-12 16:46:48 +0100

    Created boiler template for the whole app

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <span className="header">🎥 Media Entertainment 🎥 </span>
+    </div>
+  );
+};
+
+export default Header;
