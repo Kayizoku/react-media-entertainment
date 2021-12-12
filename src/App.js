d7615fda50ab487299fb94e4fd9f3118b19b409c
Mohammad Tayyab Khalid <tayyabkhalid306@gmail.com>
2021-12-12 18:55:13 +0100

    Working on routing functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import SimpleBottomNavigation from "./components/MainNav";
 import "./components/BottomNavigation.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Container, Switch } from "@mui/material";
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <div className="app">FFFFF</div>
+      <div className="app">
+        <Container>
+          <Switch>
+            <Route path="/" component={Trending} exact></Route>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/shows" component={shows}></Route>
+            <Route path="/search" component={Search}></Route>
+          </Switch>
+        </Container>
+      </div>
+
       <SimpleBottomNavigation className="bottomNavigation" />
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -2,15 +2,16 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import RestoreIcon from "@mui/icons-material/Restore";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import MovieIcon from "@mui/icons-material/Movie";
+import SearchIcon from "@mui/icons-material/Search";
+import TvIcon from "@mui/icons-material/Tv";
 
 export default function SimpleBottomNavigation() {
   const [value, setValue] = React.useState(0);
 
   return (
-    <Box sx={{}}>
+    <Box sx={{ color: "white !important" }}>
       <BottomNavigation
         className="bottomNavigation"
         showLabels
@@ -19,9 +20,26 @@ export default function SimpleBottomNavigation() {
           setValue(newValue);
         }}
       >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        <BottomNavigationAction
+          className="bottomNavigationAction"
+          label="Trending"
+          icon={<WhatshotIcon />}
+        />
+        <BottomNavigationAction
+          className="bottomNavigationAction"
+          label="Movies"
+          icon={<MovieIcon />}
+        />
+        <BottomNavigationAction
+          className="bottomNavigationAction"
+          label="TV Shows"
+          icon={<TvIcon />}
+        />
+        <BottomNavigationAction
+          className="bottomNavigationAction"
+          label="Search"
+          icon={<SearchIcon />}
+        />
       </BottomNavigation>
     </Box>
   );
