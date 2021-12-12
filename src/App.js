eb6acd671bf5c54444c141293e95867468ccc343
Mohammad Tayyab Khalid <tayyabkhalid306@gmail.com>
2021-12-12 18:29:28 +0100

    Implementing material-ui bottom navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
-
+import SimpleBottomNavigation from "./components/MainNav";
+import "./components/BottomNavigation.css";
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
-    </div>
+      <div className="app">FFFFF</div>
+      <SimpleBottomNavigation className="bottomNavigation" />
+    </>
   );
 }
 
