f646b80565f117dac967fe0b4b758115d4fd2c9f
Mohammad Tayyab Khalid <tayyabkhalid306@gmail.com>
2021-12-12 20:43:27 +0100

    Navigation is working properly now

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useEffect } from "react";
 import Box from "@mui/material/Box";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
@@ -6,9 +7,18 @@ import WhatshotIcon from "@mui/icons-material/Whatshot";
 import MovieIcon from "@mui/icons-material/Movie";
 import SearchIcon from "@mui/icons-material/Search";
 import TvIcon from "@mui/icons-material/Tv";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function SimpleBottomNavigation() {
   const [value, setValue] = React.useState(0);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (value === 0) history.push("/");
+    else if (value === 1) history.push("/movies");
+    else if (value === 2) history.push("/shows");
+    else if (value === 3) history.push("/search");
+  }, [value, history]);
 
   return (
     <Box sx={{ color: "white !important" }}>
