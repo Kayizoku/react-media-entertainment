2a0a536629e9974d72f2da20c641ac27bb8eab7c
Mohammad Tayyab Khalid <tayyabkhalid306@gmail.com>
2021-12-13 12:11:13 +0100

    fized bug was missing curly brackets for const Genres

--- src/components/Genres.js
+++ src/components/Genres.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 
-const Genres = (
+const Genres = ({
   selectedGenres,
   setSelectedGenres,
   genres,
   setGenres,
   type,
-  setpage
-) => {
+  setpage,
+}) => {
   const fetchGenres = async () => {
     const { data } = await axios.get(
       `https://api.themoviedb.org/3/genre/${type}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
