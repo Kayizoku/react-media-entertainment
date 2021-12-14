99dd38d12dc9a186c283c5c5276b69c893ecf21c
Mohammad Tayyab Khalid <tayyabkhalid306@gmail.com>
2021-12-14 08:53:58 +0100

    Started working on the Search page

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,7 +1,37 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Button, TextField } from "@mui/material";
+import { useState } from "react";
+import SearchIcon from "@material-ui/icons/Search";
+
 const Search = () => {
+  const [type, setType] = useState(0);
+  const [searchText, setSearchText] = useState("");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#fff",
+      },
+    },
+  });
+
   return (
     <div>
-      <span className="pageTitle">Search</span>
+      <ThemeProvider theme={darkTheme}>
+        <div style={{ display: "flex", margin: "15px 0" }}>
+          <TextField
+            style={{ flex: 1 }}
+            className="searchBox"
+            label="Search"
+            variant="filled"
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <Button variant="contained" style={{ marginLeft: 10 }}>
+            <SearchIcon />
+          </Button>
+        </div>
+      </ThemeProvider>
     </div>
   );
 };
