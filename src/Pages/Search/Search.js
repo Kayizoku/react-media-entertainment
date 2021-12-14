6dded94c77001d2ef92da5bc59e44a4066f7ef1b
Mohammad Tayyab Khalid <tayyabkhalid306@gmail.com>
2021-12-14 10:22:04 +0100

    Search  is now implemented

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,11 +1,18 @@
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import "./Search.css";
 import { Button, Tab, Tabs, TextField } from "@mui/material";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SearchIcon from "@material-ui/icons/Search";
+import axios from "axios";
+import MediaContent from "../../components/MediaContent/MediaContent";
+import CustomPagination from "../../components/Pagination/CustomPagination";
 
 const Search = () => {
   const [type, setType] = useState(0);
   const [searchText, setSearchText] = useState("");
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState();
+  const [numOfPages, setNumOfPages] = useState();
 
   const darkTheme = createTheme({
     palette: {
@@ -15,6 +22,27 @@ const Search = () => {
       },
     },
   });
+
+  const fetchSearch = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}?api_key=${
+          process.env.REACT_APP_API_KEY
+        }&language=en-US&query=${searchText}&page=${page}&include_adult=false`
+      );
+      setContent(data.results);
+      setNumOfPages(data.total_pages);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    window.scroll(0, 0);
+    fetchSearch();
+    // eslint-disable-next-line
+  }, [type, page]);
 
   return (
     <div>
@@ -27,16 +55,50 @@ const Search = () => {
             variant="filled"
             onChange={(e) => setSearchText(e.target.value)}
           />
-          <Button variant="contained" style={{ marginLeft: 10 }}>
+          <Button
+            variant="contained"
+            style={{ marginLeft: 10 }}
+            onClick={fetchSearch}
+          >
             <SearchIcon />
           </Button>
         </div>
 
-        <Tabs value={type} indicatorColor="primary" textColor="primary">
-          <Tab style={{ width: "50%" }} label="Search Movies"></Tab>
-          <Tab style={{ width: "50%" }} label="Search TV Shows"></Tab>
+        <Tabs
+          className="tabs-container"
+          value={type}
+          indicatorColor="primary"
+          textColor="primary"
+          onChange={(event, newValue) => {
+            setType(newValue);
+            setPage(1);
+          }}
+        >
+          <Tab className="tab" label="Search Movies"></Tab>
+          <Tab className="tab" label="Search TV Shows"></Tab>
         </Tabs>
       </ThemeProvider>
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <MediaContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              overview={c.overview}
+              date={c.release_date || c.first_air_date}
+              media_type={type ? "tv" : "movie"}
+              vote_average={c.vote_average}
+            />
+          ))}
+        {searchText &&
+          !content &&
+          (type ? <h2>No TV Shows Found</h2> : <h2>No Movies Found</h2>)}
+      </div>
+      {numOfPages > 1 && (
+        <CustomPagination setPage={setPage} numOfPages={numOfPages} />
+      )}
     </div>
   );
 };
